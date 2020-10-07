c1b4ea90fdef41043cd1bde609727be77763b966
Singgih Praditya <singgihpraditya@gmail.com>
2020-10-07 15:02:25 +0700

    revisi

--- spring-boot-rest/src/main/java/com/singgih/spring/boot/controller/ProductTestController.java
+++ spring-boot-rest/src/main/java/com/singgih/spring/boot/controller/ProductTestController.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
